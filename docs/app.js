085d42da73bbba9973790e0230330c127a774c36
bb <26394523+broeknbytes@users.noreply.github.com>
2026-06-17 08:09:58 -0700

    docs: switch import path to local development version

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-import { initTable, linkCell } from 'https://cdn.jsdelivr.net/gh/amazebb/amazejs@v0.2.0/src/index.js';
+import { initTable, linkCell } from '../../amazejs/src/index.js';
 
 function copyBrewInstall(visibleItems, btn) {
     const formulas = [], casks = [];
